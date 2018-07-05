8e23a03db6d70469b4bcc665b4ebe6bb02eb9b58
Nick Nazari <nhn2915@gmail.com>
2018-07-05 18:16:24 -0400

    Separated classes into different files to make changes easier.

---
+++ environment.py
@@ -0,0 +1,10 @@
+import species
+
+# two parts
+# 1. the environment
+# 2. the testAgainst(species,environment) function
+
+class Environment:
+	def __init__(self,difficulty,modifier):
+		self.difficulty = difficulty
+		self.modifier = modifier
---
+++ species.py
@@ -0,0 +1,63 @@
+import random
+import environment
+
+class Species:
+
+	def __init__(self,agility,intimidation,strength,aggressiveness,fly,swim,endurance,intelligence,camoflague,size):
+		self.agility =  agility
+		self.intimidation = intimidation
+		self.strength = strength
+		self.aggressiveness = aggressiveness
+		self.fly = fly
+		self.swim = swim
+		self.endurance = endurance
+		self.intelligence = intelligence
+		self.camoflague = camoflague
+		self.size = size
+
+		self.stats = {
+			'Agility': agility,
+			'Intimidation': intimidation,
+			'Strength': strength,
+			'Aggressiveness': aggressiveness,
+			'Fly': fly,
+			'Swim': swim,
+			'Endurance': endurance,
+			'Intelligence': intelligence,
+			'Camoflague': camoflague,
+			'Size': size					
+			}
+
+		# the name is defined as the highest value above
+
+		self.max = max(self.stats, key=self.stats.get)
+
+	def __str__(self):
+		return "Name: %s\nAgility: %f\nIntimidation: %f\nStrength: %f\nAggressiveness: %f\nFly: %r\nSwim: %r\nEndurance: %f\nIntelligence: %f\nCamoflague: %f\nSize: %f" % (self.max, self.agility, self.intimidation, self.strength, self.aggressiveness, bool(self.fly), bool(self.swim), self.endurance, self.intelligence, self.camoflague, self.size)
+
+	def genRandomSpecies():
+		return Species(random.uniform(0,1),random.uniform(0,1),random.uniform(0,1),random.uniform(0,1),0.1,0.1,random.uniform(0,1),random.uniform(0,1),random.uniform(0,1),random.uniform(0,1))
+
+	def getAvgStat(data,stat):
+		total = 0
+		amount = 0
+		for entry in data:
+			total += entry.stats.get(stat)
+			amount += 1
+			
+		average = total / amount
+
+		return average
+
+if __name__ == '__main__':
+
+	testSpecies = Species(0.24,0.45,0.3,0.12,.1,.1,0.89,0.46,0.26,0.33)
+	# all max values
+
+	collection = []
+
+	for num in range(0,100):
+		collection.append(Species.genRandomSpecies())
+
+	print(Species.getAvgStat(collection,'Size'))
+
